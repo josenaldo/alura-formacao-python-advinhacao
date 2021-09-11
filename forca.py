7fc0bdc2126b96874c4d756de87df7133b1b1fa4
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-09-11 14:04:13 -0300

    Aula de manipulação de strings

--- forca.py
+++ forca.py
@@ -9,6 +9,15 @@ def jogar():
     acertou = False
 
     while(not acertou and not enforcou):
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print(f'Encotrei letra {letra} posição {index}')
+            index = index + 1
         print("Jogando...")
 
     print("Fim do jogo")
